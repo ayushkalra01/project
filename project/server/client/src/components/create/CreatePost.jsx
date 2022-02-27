a9d46538cfa20277a6dbf69397e262380451e4b7
Ayushkalra01 <aayush.kkalra30@gmail.com>
2022-02-27 22:25:00 +0530

    updated

--- project/server/client/src/components/create/CreatePost.jsx
+++ project/server/client/src/components/create/CreatePost.jsx
@@ -61,27 +61,7 @@ const CreatePost = () => {
 	const [file, setFile] = useState('');
 	const [imageURL, setImageURL] = useState('');
 
-	// const url = post.picture
-	// 	? post.picture
-	// 	: 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
-
-	useEffect(() => {
-		const getImage = async () => {
-			if (file) {
-				const data = new FormData();
-				data.append('name', file.name);
-				data.append('file', file);
-
-				const image = await uploadFile(data);
-				post.picture = image.data;
-				setImageURL(image.data);
-			}
-		};
-		getImage();
-		post.categories = location.search?.split('=')[1] || 'All';
-		post.username = user.email.split('@')[0];
-	}, [file]);
-
+	post.username = user.email.split('@')[0];
 	const savePost = async () => {
 		await createPost(post);
 		history.push('/');
